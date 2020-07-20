80a0884d0966e5609d84dc6d996287b5cd4e652a
HEMANT RANJAN <41506705+hemantrk2122@users.noreply.github.com>
2020-07-20 20:23:27 +0530

    Update 14Angle Between Hands of a Clock.cpp

--- 14Angle Between Hands of a Clock.cpp
+++ 14Angle Between Hands of a Clock.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
-    string reverseWords(string s) {
-        stringstream all(s); 
-        string word,ans = "";
-        while (all >> word)
-            ans = word + " " + ans;
-        return ans.substr(0,ans.length()-1);
+    double angleClock(int h, int m) {
+        float hour=(h%12+(float)m/60)*30;
+        float min=m*6;
+        float angle=abs(min-hour);
+        if(angle>180)angle=360-angle;
+        return angle;
+        
     }
 };
