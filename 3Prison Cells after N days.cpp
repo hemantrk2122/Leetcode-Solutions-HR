01e8b7bde0fcb2518a4629516df9b670b59432d2
HEMANT RANJAN <41506705+hemantrk2122@users.noreply.github.com>
2020-07-20 20:16:01 +0530

    Update 3Prison Cells after N days.cpp

--- 3Prison Cells after N days.cpp
+++ 3Prison Cells after N days.cpp
@@ -1,33 +1,29 @@
 class Solution {
 public:
-    int nthUglyNumber(int n) {
-        int u[n];
-        int i2=0,i3=0,i5=0;
-        int n2=2;
-        int n3=3;
-        int n5=5;
-
-        int nu=1;
-        u[0]=1;
-        for(int i=1;i<n;i++){
-            nu=min(n2,min(n3,n5));
-
-            u[i]=nu;
-            if(nu==n2){
-                i2++;
-                n2=u[i2]*2;
+    vector<int> prisonAfterNDays(vector<int>& cells, int N) {
+        unordered_map<string,int>m;
+        
+        for(int i=0;i<N;i++){
+            string s(cells.begin(),cells.end());
+            if(m.find(s)!=m.end()){
+                int loop_length=i-m[s];
+                int remaining_days=(N-i)%loop_length;
+                return(prisonAfterNDays(cells,remaining_days));
             }
-            if(nu==n3){
-                i3++;
-                n3=u[i3]*3;
+            else
+            {
+                m.insert({s,i});
+                int prev=cells[0];
+                for(int j=1;j<7;j++){
+                    int next=cells[j+1];
+                    int cur=cells[j];
+                    cells[j]= prev == next;
+                    prev=cur;
+                }
             }
-            if(nu==n5){
-                i5++;
-                n5=u[i5]*5;
-            }
-
-
+            cells[0]=0;
+            cells[7]=0;
         }
-        return nu;
+        return cells;
     }
 };
