641c2b5e4b0b596d57d175f4215d375b2e17a38c
HEMANT RANJAN <41506705+hemantrk2122@users.noreply.github.com>
2020-07-20 20:16:45 +0530

    Update 4Ugly number II.cpp

--- 4Ugly number II.cpp
+++ 4Ugly number II.cpp
@@ -1,15 +1,33 @@
 class Solution {
 public:
-    int hammingDistance(int x, int y) {
-        int n= x ^y;
-    int setBits = 0; 
-  
-    while (n> 0) { 
-        setBits += n & 1;
-        n>>= 1; 
-    } 
-  
-    return setBits; 
-        
+    int nthUglyNumber(int n) {
+        int u[n];
+        int i2=0,i3=0,i5=0;
+        int n2=2;
+        int n3=3;
+        int n5=5;
+
+        int nu=1;
+        u[0]=1;
+        for(int i=1;i<n;i++){
+            nu=min(n2,min(n3,n5));
+
+            u[i]=nu;
+            if(nu==n2){
+                i2++;
+                n2=u[i2]*2;
+            }
+            if(nu==n3){
+                i3++;
+                n3=u[i3]*3;
+            }
+            if(nu==n5){
+                i5++;
+                n5=u[i5]*5;
+            }
+
+
+        }
+        return nu;
     }
 };
