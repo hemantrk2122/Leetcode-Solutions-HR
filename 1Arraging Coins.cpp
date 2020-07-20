19d7d444ce48ad4e6f4ba5892a83a467bc0eb1da
HEMANT RANJAN <41506705+hemantrk2122@users.noreply.github.com>
2020-07-20 20:13:25 +0530

    Update 1Arraging Coins.cpp

--- 1Arraging Coins.cpp
+++ 1Arraging Coins.cpp
@@ -1,46 +1,18 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    vector<vector<int>> levelOrderBottom(TreeNode* root) {
-        if(root==NULL)return vector<vector<int>>();
-           
-        queue <TreeNode*> q;
-        TreeNode* cur;
-        vector<int>level;vector<vector<int>>ans;
-        int levelCount=0;
-        
-        q.push(root);
-        levelCount++;
-        
-        while(!q.empty()){
-            cur=q.front();
-            q.pop();
-            levelCount--;
-            
-            level.push_back(cur->val);
-            
-            if(cur->left) q.push(cur->left);
-            if(cur->right) q.push(cur->right);
-            
-            if(levelCount==0){
-                ans.push_back(level);
-                level=vector<int>();
-                
-                levelCount=q.size();
+    int arrangeCoins(int n) {
+        int i=1;
+
+        while(n>=0){
+            if(n>0){
+                n-=i;
             }
-            
+            i++;
         }
-        reverse(ans.begin(),ans.end());
-        return ans;
+        //cout<<n<<endl;
+
+        return i-1;
+
     }
+
 };
