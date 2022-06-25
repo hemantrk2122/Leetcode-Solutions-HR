f5020988c6375b5c83ec85a7e56a64b49e9261fe
HEMANT RANJAN <41506705+hemantrk2122@users.noreply.github.com>
2022-06-25 19:55:02 +0530

    Time: 3 ms (62.09%), Space: 7 MB (92.51%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -1,11 +1,33 @@
 class Solution {
 public:
-    void rotate(vector<vector<int>>& m) {
-        reverse(m.begin(),m.end());
-        for(int i = 0;i<m.size();i++){
-            for(int j = i+1;j<m[i].size();j++){
-                swap(m[i][j],m[j][i]);
+    void transpose(vector<vector<int>> &a){
+        for(int i = 0;i<a.size();i++){
+            for(int j = i+1;j<a[i].size();j++){
+                swap(a[i][j],a[j][i]);
             }
         }
+    }
+    void reverse(vector<vector<int>>& a){
+        int n = a.size();
+        for(int row = 0; row<n; row++){
+            int i = 0;
+            int j = n-1;
+            while(i<j){
+                swap(a[row][i++],a[row][j--]);
+            }
+            
+        }
+        // for(int i = 0;i<a.size();i++){
+        //     for(int j = 0;j<a[i].size()/2;j++){
+        //         int n = a[i].size();
+        //         swap(a[i][j],a[j][n-j-1]);
+        //     }
+        // }
+    }
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        transpose(matrix);
+        reverse(matrix);
+        
     }
 };
