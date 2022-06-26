628b7a3a8fdf61b9ab98c45296fb3b58ae8d06b0
HEMANT RANJAN <41506705+hemantrk2122@users.noreply.github.com>
2022-06-26 21:26:06 +0530

    Time: 139 ms (37.50%), Space: 14.9 MB (23.66%) - LeetHub

--- 18-4sum/18-4sum.cpp
+++ 18-4sum/18-4sum.cpp
@@ -1,47 +1,30 @@
 class Solution {
 public:
-
-vector<vector<int>> fourSum(vector<int>& v, int t) {
-    sort(begin(v),end(v));
-    int n=v.size();
-    vector<vector<int>>ans;
-    long long int tt=t;
-    for(int i=0;i<n;i++)
-    {
-        for(int j=i+1;j<n;j++)
-        {
-            tt-=(v[i]+v[j]);
-            int l=j+1;
-            int r=n-1;
-            
-            while(l<r)
-            {
-                if(tt==v[l]+v[r])
-                {
-                    ans.push_back({v[i],v[j],v[l],v[r]});
-                    while(l<r and v[l]==v[l+1])l++;
-                    while(l<r and v[r]==v[r-1])r--;
-                    
-                    l++;
-                    r--;
-                    
-                }
-                else if(v[l]+v[r]<tt)
-                {
-                    l++;
-                }
-                else 
-                {
-                    r--;
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        sort(nums.begin(),nums.end());
+        set<vector<int>>ans;
+        int n = nums.size();
+        for(int i = 0;i<n-1;i++){
+            for(int j = i+1; j<n;j++){
+                long long new_target = (long long)target - nums[i] - nums[j];
+                int k = j+1;
+                int l = n-1;
+                while(k<l){
+                    long long sum = (nums[k]+nums[l]);
+                    if(sum == new_target){
+                        ans.insert({nums[i],nums[j],nums[k],nums[l]});
+                        while(k<l && nums[k] == nums[k+1])k++;
+                        while(k<l && nums[l] == nums[l-1])l--;
+                        k++;
+                        l--;
+                    }
+                    else if(sum>new_target)l--;
+                    else k++;
                 }
             }
-            tt+=v[i]+v[j];
-        while(j+1<n and v[j]==v[j+1])j++;
         }
-        while(i+1<n and v[i]==v[i+1])i++;
+        vector<vector<int>>res;
+        for(auto ele:ans)res.push_back(ele);
+        return res;
     }
-    
-    return ans ;
-    
-}
 };
