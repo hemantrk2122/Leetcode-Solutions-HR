7056372549ef8f1881f7b042b0ba802bc5d7784b
HEMANT RANJAN <41506705+hemantrk2122@users.noreply.github.com>
2022-06-27 00:24:30 +0530

    Time: 2507 ms (5.00%), Space: 51.1 MB (22.23%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
@@ -1,38 +1,38 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        set<int>s;
-        for(auto z:nums){
-            s.insert(z);
-        }
-        int ls=0;
-        for(auto z:nums){
-            if(!s.count(z-1)){
-                int cn=z;
-                int cs=1;
-                while(s.count(cn+1)){
-                    cn+=1;
-                    cs+=1;
-                }
-                ls=max(ls,cs);
+//         set<int>s;
+//         for(auto z:nums){
+//             s.insert(z);
+//         }
+//         int ls=0;
+//         for(auto z:nums){
+//             if(!s.count(z-1)){
+//                 int cn=z;
+//                 int cs=1;
+//                 while(s.count(cn+1)){
+//                     cn+=1;
+//                     cs+=1;
+//                 }
+//                 ls=max(ls,cs);
                 
+//             }
+//         }
+//         return ls;
+        set<int>s;
+        for(auto num:nums)s.insert(num);
+        int ans = 0;
+        for(auto num:nums){
+            if(!s.count(num-1)){
+                int currNum = num;
+                int currStreak = 1;
+                while(s.count(++num)){
+                    currNum++;
+                    currStreak++;
+                }
+                ans = max(ans, currStreak);
             }
         }
-        return ls;
-        // set<int>s;
-        // for(auto num:nums)s.insert(num);
-        // int ans = 0;
-        // for(auto num:nums){
-        //     if(!s.count(num-1)){
-        //         int currNum = num;
-        //         int currStreak = 1;
-        //         while(s.count(++num)){
-        //             currNum++;
-        //             currStreak++;
-        //         }
-        //         ans = max(ans, currStreak);
-        //     }
-        // }
-        // return ans;
+        return ans;
     }
 };
