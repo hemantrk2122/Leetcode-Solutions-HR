54ef8df25b4cb67c7c9adf51c014b270aec633c7
HEMANT RANJAN <41506705+hemantrk2122@users.noreply.github.com>
2022-06-29 23:30:03 +0530

    Time: 28 ms (48.50%), Space: 8.4 MB (57.88%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -2,14 +2,27 @@ class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
         unordered_map<int,int>mp;
-        int n = s.size();
-        int i = 0;
-        int ans = 0;
-        for(int j = 0;j<n;j++){
-            if(mp.find(s[j])!=mp.end())i = max(i,mp[s[j]]+1);
-            mp[s[j]]=j;
-            ans = max(ans,j-i+1);
+        int i = 0, count = 0;
+        for(int j = 0;j<s.size();j++){
+            if(mp.find(s[j])!=mp.end()){
+                i = max(i,mp[s[j]]+1);
+            }
+            mp[s[j]] = j;
+            count = max(count,j-i+1);
         }
-        return ans;
+        return count;
     }
 };
+
+//count = 0->1->2
+//[a,b,c,a,b,c,b,b]
+//[0,1,2,3,4,5,6,7]
+
+//i->starting index of req subarr
+//j -> iter
+
+//i = 0, j = 2
+
+//count = max(count, j-i+1);
+//if(ele-> found in map)
+//I should have a potential starting point of new subarr
