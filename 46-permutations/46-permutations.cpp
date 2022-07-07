50bec1326a99cef7cabbb0b86842fef4d4bae123
HEMANT RANJAN <41506705+hemantrk2122@users.noreply.github.com>
2022-07-07 20:49:17 +0530

    Time: 9 ms (22.90%), Space: 7.7 MB (78.24%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
-    void solve(int i, vector<int>&nums, int n, vector<vector<int>> &ans, vector<int>&temp){
-        cout<<i<<endl;
-        if(temp.size()==n){
-            ans.push_back(temp);
+    void solve(int i, vector<int>&nums, vector<vector<int>> &ans){
+        // cout<<i<<endl;
+        if(i+1==nums.size()){
+            ans.push_back(nums);
             return;
         }
         
-            solve((i+1)%n,nums,n,ans,temp);
-            temp.push_back(nums[i]);
-            solve((i+1)%n,nums,n,ans,temp);
-            temp.pop_back();
-            return;
+            for(int j = i;j<nums.size();j++){
+                swap(nums[i],nums[j]);
+                solve(i+1,nums,ans);
+                swap(nums[i],nums[j]);
+            }
     }
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>>ans;
-        sort(nums.begin(),nums.end());
-        do{
-            ans.push_back(nums);
-        }while(next_permutation(nums.begin(),nums.end()));
+        solve(0,nums,ans);
         return ans;
     }
 };
