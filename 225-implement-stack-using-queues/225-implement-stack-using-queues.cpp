29ca84f13405cffa183779f0692198126a253ba5
HEMANT RANJAN <41506705+hemantrk2122@users.noreply.github.com>
2022-07-17 12:56:23 +0530

    Time: 3 ms (42.42%), Space: 6.9 MB (57.28%) - LeetHub

--- 225-implement-stack-using-queues/225-implement-stack-using-queues.cpp
+++ 225-implement-stack-using-queues/225-implement-stack-using-queues.cpp
@@ -1,35 +1,38 @@
 class MyStack {
 public:
-    queue<int>q1;
-    queue<int>q2;
+    queue<int>q;
     MyStack() {
         
     }
     
     void push(int x) {
-        q2.push(x);
-        while(q1.size()!=0){
-            q2.push(q1.front());
-            q1.pop();
+        q.push(x);
+        // while(q1.size()!=0){
+        //     q2.push(q1.front());
+        //     q1.pop();
+        // }
+        for(int i = 0;i<q.size()-1;i++){
+            q.push(q.front());
+            q.pop();
         }
-        queue<int>temp=q1;
-        q1=q2;
-        q2=temp;
+        // queue<int>temp=q1;
+        // q1=q2;
+        // q2=temp;
     }
     
     int pop() {
-        int x = q1.front();
-        q1.pop();
+        int x = q.front();
+        q.pop();
         return x;
     }
     
     int top() {
-        return q1.front();
+        return q.front();
         
     }
     
     bool empty() {
-        return q1.size()==0;
+        return q.size()==0;
     }
 };
 
