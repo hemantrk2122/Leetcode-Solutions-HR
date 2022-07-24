8d489fdfa1f2dd031f022012e7b03b2d9b74fff6
HEMANT RANJAN <41506705+hemantrk2122@users.noreply.github.com>
2022-07-24 22:54:16 +0530

    Time: 147 ms (65.66%), Space: 14.9 MB (21.86%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cpp
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cpp
@@ -6,7 +6,6 @@ public:
         
         int i = 0, j = n-1;
         while(i<m && j>=0){
-            // cout<<i<<" "<<j<<endl;
             if(mat[i][j]==tar)return true;
             else if(mat[i][j]>tar)j--;
             else
