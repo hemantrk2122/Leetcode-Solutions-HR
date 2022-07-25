cfb656a0db1a635ff4de923154fb82a419af18cf
HEMANT RANJAN <41506705+hemantrk2122@users.noreply.github.com>
2022-07-25 13:04:43 +0530

    Time: 8 ms (84.21%), Space: 14.2 MB (5.65%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,11 +1,28 @@
 class Solution {
 public:
+    int find(vector<int>nums,int tar, bool first){
+        int low = 0;
+        int high = nums.size()-1;
+        int ans = -1;
+        
+        while(low<=high){
+            int mid = low + (high - low)/2;
+            if(nums[mid] == tar){
+                ans = mid;
+                if(first)high = mid-1;
+                else
+                    low = mid+1;
+            }
+            else if(nums[mid]>tar)high = mid-1;
+            else
+                low = mid+1;
+        }
+        return ans;
+    }
     vector<int> searchRange(vector<int>& nums, int target) {
-        auto low = lower_bound(nums.begin(),nums.end(),target);
-        auto high = upper_bound(nums.begin(),nums.end(),target);
-        
-        if(!binary_search(nums.begin(),nums.end(),target))return {-1,-1};
-        
-        return {int(low-nums.begin()), int(high-nums.begin()-1)};
+        int stInd = find(nums,target,true);
+        if(stInd == -1)return {-1,-1}; 
+        int ltInd = find(nums,target,false);
+        return {stInd,ltInd};
     }
 };
