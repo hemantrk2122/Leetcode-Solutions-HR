e1748e836b6c2ff072108185ce985dc9a5743704
HEMANT RANJAN <41506705+hemantrk2122@users.noreply.github.com>
2022-08-01 11:05:51 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (46.14%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,27 +1,19 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        //(n+m-1)C(m-1)
-        int a[m+1][n+1];
-        memset(a, 0, sizeof(a));
+        vector<vector<int>>grid(m+1,vector<int>(n+1));
         for(int i = 0;i<n;i++){
-            a[m-1][i] = 1;
+            grid[m-1][i] = 1;
         }
         for(int i = 0;i<m;i++){
-            a[i][n-1] = 1;
+            grid[i][n-1] = 1;
         }
         
         for(int i = m-2;i>=0;i--){
             for(int j = n-2;j>=0;j--){
-                a[i][j] = a[i+1][j] + a[i][j+1];
+                grid[i][j] = grid[i][j+1] + grid[i+1][j];
             }
         }
-        for(int i = 0;i<m;i++){
-            for(int j = 0;j<n;j++){
-                cout<<a[i][j]<<" ";
-            }
-            cout<<endl;
-        }
-        return a[0][0];
+        return grid[0][0];
     }
 };
