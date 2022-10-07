77dd5cb1727f31e23aede214db492f812895e5a4
HEMANT RANJAN <41506705+hemantrk2122@users.noreply.github.com>
2022-10-08 00:06:50 +0545

    Time: 13 ms (40.35%), Space: 42.8 MB (98.71%) - LeetHub

--- 463-island-perimeter/463-island-perimeter.java
+++ 463-island-perimeter/463-island-perimeter.java
@@ -3,22 +3,20 @@ class Solution {
         int n = grid.length;
         int m = grid[0].length;
         
-        boolean[][] vis = new boolean[n][m];
         int ans = 0;
         for(int i = 0;i<n;i++){
             for(int j = 0;j<m;j++){
-                if(!vis[i][j] && grid[i][j] == 1){
-                    ans+=dfs(grid,i,j,vis);        
+                if(grid[i][j] == 1){
+                    ans+=dfs(grid,i,j);        
                 }
             }
         }
         return ans;
         
     }
-    int dfs(int[][] grid, int i,int j, boolean[][] vis){
+    int dfs(int[][] grid, int i,int j){
         int n = grid.length;
         int m = grid[0].length;
-        // vis[i][j] = true;
         
         int count = 0;
         int[][] dirs = {{0,1},{0,-1},{1,0},{-1,0}};
@@ -27,12 +25,10 @@ class Solution {
             int x = i + dir[0];
             int y = j + dir[1];
             
-            if(x>=0 && x<n && y>=0 && y<m && !vis[x][y] && grid[x][y] == 1){
+            if(x>=0 && x<n && y>=0 && y<m && grid[x][y] == 1){
                 count++;
-                // dfs(grid,x,y,vis);
             }
         }
-        // System.out.println(count);
         return 4-count;
     }
 }
