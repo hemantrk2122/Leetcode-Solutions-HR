5f4b2abda7aae11840258715acfab75c5b81252a
HEMANT RANJAN <41506705+hemantrk2122@users.noreply.github.com>
2022-10-11 07:18:17 +0530

    Time: 0 ms (100.00%), Space: 40.2 MB (92.69%) - LeetHub

---
+++ 1328-break-a-palindrome/1328-break-a-palindrome.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String breakPalindrome(String palindrome) {
+        char[] cstr=palindrome.toCharArray();
+        if(cstr.length<2){
+            return "";
+        }
+        for(int i=0;i<cstr.length/2;i++){
+            if(cstr[i]!='a'){
+                cstr[i]='a';
+                return String.valueOf(cstr);
+            }
+        }
+        cstr[cstr.length-1]='b';
+        return String.valueOf(cstr);
+    }
+}
